fc06a5f2716f0cd02c2de72882958e5cdd02b202
Luis Gizirian <v-lugizi@microsoft.com>
2017-10-20 19:40:12 +0000

    Initial upload. WIP yet.

---
+++ BackEnd.Core.Stateful/Controllers/AnalysisController.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.ServiceFabric.Data;
+using Microsoft.ServiceFabric.Data.Collections;
+
+namespace BackEnd.Core.Stateful.Controllers
+{
+    [Route("api/[controller]")]
+    public class AnalysisController : Controller
+    {
+        private readonly IReliableStateManager _stateManager;
+
+        public AnalysisController(
+            IReliableStateManager stateManager)
+        {
+            _stateManager = stateManager;
+        }
+
+        [HttpPost]
+        public async Task MergeSurveyAnswerToAnalysisAsync([FromBody]string slot, [FromBody]string response)
+        {
+            try
+            {
+                var reliableDictionary = await _stateManager.GetOrAddAsync<IReliableDictionary<string, int>>("reliableDictionary");
+
+                using (var tx = _stateManager.CreateTransaction())
+                {
+                    var conditional = await reliableDictionary.TryGetValueAsync(tx, slot);
+                    var accumulated = 0;
+
+                    if (!conditional.HasValue)
+                    {
+                        accumulated += 1;
+                    }
+                    else
+                    {
+                        accumulated = conditional.Value + 1;
+                    }
+
+                    ServiceEventSource.Current.Message("Slug name:{0}|Total answers:{1}", slot,
+                        accumulated);
+
+                    await reliableDictionary.AddOrUpdateAsync(tx, slot, accumulated, (key, element) => {
+                        return element;
+                    });
+
+                    await tx.CommitAsync();
+                }
+            }
+            catch (Exception ex)
+            {
+                ServiceEventSource.Current.ServiceRequestFailed(ex.ToString());
+                throw;
+            }
+        }
+
+        [HttpGet]
+        [Route("summary/{slot}")]
+        public async Task<int> GetSurveyAnswersSummaryAsync(string slot)
+        {
+            try
+            {
+                var reliableDictionary = await _stateManager.GetOrAddAsync<IReliableDictionary<string, int>>("reliableDictionary");
+                var result = 0;
+
+                using (var tx = _stateManager.CreateTransaction())
+                {
+                    var conditional = await reliableDictionary.TryGetValueAsync(tx, slot);
+
+                    if (conditional.HasValue)
+                    {
+                        result = conditional.Value;
+                    }
+                    await tx.CommitAsync();
+                }
+
+                return result;
+            }
+            catch (Exception ex)
+            {
+                ServiceEventSource.Current.ServiceRequestFailed(ex.ToString());
+                throw;
+            }
+        }
+    }
+}
---
+++ BackEnd.Core.Stateless/Controllers/AnalysisController.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using StackExchange.Redis;
+using Newtonsoft.Json;
+using BackEnd.Core.Stateless.Helpers;
+
+namespace BackEnd.Core.Stateless.Controllers
+{
+    [Route("api/[controller]")]
+    public class AnalysisController : Controller
+    {
+        private static Lazy<ConnectionMultiplexer> lazyConnection = new Lazy<ConnectionMultiplexer>(() =>
+        {
+            string cacheConnection = ServiceFabricConfiguration.GetConfigurationSettingValue("ConnectionStrings", "RedisCacheConnectionString", "YourRedisCacheConnectionString");
+
+            return ConnectionMultiplexer.Connect(cacheConnection);
+        });
+
+        public static ConnectionMultiplexer Connection
+        {
+            get
+            {
+                return lazyConnection.Value;
+            }
+        }
+
+        [HttpPost]
+        public async Task MergeSurveyAnswerToAnalysisAsync([FromBody]string slot, [FromBody]string response)
+        {
+            try
+            {
+                var surveyAnswersSummaryCache = Connection.GetDatabase();
+                var success = false;
+
+                do
+                {
+                    var result = await surveyAnswersSummaryCache.StringGetAsync(slot);
+                    var isNew = result.IsNullOrEmpty;
+                    var transaction = surveyAnswersSummaryCache.CreateTransaction();
+
+                    var accumulated = 0;
+
+                    if (isNew)
+                    {
+                        transaction.AddCondition(Condition.KeyNotExists(slot));
+                        accumulated += 1;
+                    }
+                    else
+                    {
+                        accumulated = JsonConvert.DeserializeObject<int>(result);
+                        transaction.AddCondition(Condition.StringEqual(slot, ++accumulated));
+                    }
+
+                    ServiceEventSource.Current.Message("Slug name:{0}|Total answers:{1}", slot,
+                        accumulated);
+
+                    transaction.StringSetAsync(slot,
+                            JsonConvert.SerializeObject(accumulated));
+
+                    //This is a simple implementation of optimistic concurrency.
+                    //If transaction fails, another user must have edited the same survey.
+                    //If so, try to process this survey answer again.
+
+                    //Another approach is to store each survey answer option as a separate hash in Redis and simply increment
+                    //the hash value. This technique will not cause collisions with other threads but will require a redesign of
+                    //how survey answer summaries are stored in redis. This approach is left to the reader to implement.
+
+                    success = await transaction.ExecuteAsync();
+                } while (!success);
+
+            }
+            catch (Exception ex)
+            {
+                ServiceEventSource.Current.ServiceRequestFailed(ex.ToString());
+                throw;
+            }
+        }
+
+        [HttpGet]
+        [Route("summary/{slot}")]
+        public async Task<int> GetSurveyAnswersSummaryAsync(string slot)
+        {
+            try
+            {
+                var surveyAnswersSummaryCache = Connection.GetDatabase();
+
+                // Look for slug name in the survey answers summary cache
+                var surveyAnswersSummaryInStore = await surveyAnswersSummaryCache.StringGetAsync(slot);
+
+                var returnData = 0;
+
+                if (!surveyAnswersSummaryInStore.IsNullOrEmpty)
+                {
+                    returnData =
+                        JsonConvert.DeserializeObject<int>(surveyAnswersSummaryInStore);
+                }
+
+                return returnData;
+            }
+            catch (Exception ex)
+            {
+                ServiceEventSource.Current.ServiceRequestFailed(ex.ToString());
+                throw;
+            }
+        }
+    }
+}
